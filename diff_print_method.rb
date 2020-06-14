951ee010a9da782017f91b6c72d03f5ad68f16c7
KarGop <66565340+KarGop@users.noreply.github.com>
2020-06-14 13:44:41 +0530

    Diff_print

---
+++ diff_print_method.rb
@@ -0,0 +1,17 @@
+#Print method: No line break
+print "Hello"  #Hello
+print "World" #HelloWorld
+print "\n"   #cursor will move to next line
+
+#puts method: Will add line break at the end
+puts "Hello"  #Hello
+puts "World"   #World
+puts  #new line
+
+#p method: output with Double_quotes
+p "Hello world!" #"Hello world!"
+p "Hi there, this is a
+big line break"  #"Hi there, this is a\nbig line break"
+puts "Hi there, this is a
+big line break"  #Hi there, this is a
+                  #big line break
